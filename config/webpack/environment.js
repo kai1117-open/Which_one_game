f911cfd0c33bc921e7e277bf4013d7b4aafe3350
kai1117-open <kai1117-open@users.noreply.github.com>
2025-01-18 05:09:04 +0000

    環境構築のエラー解消

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,26 @@
 const { environment } = require('@rails/webpacker')
 
+// 不要な `node` 設定を削除します
+environment.config.node = {
+  // 必要に応じて以下の設定を有効にしますが、基本的には削除で問題解決する場合が多いです
+  __dirname: true,
+  __filename: true,
+  global: true
+}
+
+const babelLoader = environment.loaders.get('babel')
+babelLoader.use[0].options.presets = ['@babel/preset-env']
+
+
 module.exports = environment
+
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+)
